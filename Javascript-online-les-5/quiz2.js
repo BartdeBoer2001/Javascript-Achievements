38fe1c9376152259062a51587bd0cfdf00c0feec
Bart <Bartdb2001@gmail.com>
2020-12-17 10:12:14 +0100

    Opgave quiz

---
+++ Javascript-online-les-5/quiz2.js
@@ -0,0 +1,61 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Rekenvragen",
+        "imageURI": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x3y3z3k-1568041112.jpg?crop=0.987xw:0.790xh;0.00160xw,0.128xh&resize=980:*"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is 2/3 + 4/6?",
+				"answers": [{
+						"answer": "1 2/3",
+						"feedback": false
+					},
+					{
+						"answer": "1",
+						"feedback": false
+					},
+					{
+						"answer": "1 1/3",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "Wat is 4 * 69?",
+				"answers": [{
+						"answer": "236",
+						"feedback": false
+					},
+					{
+						"answer": "279",
+						"feedback": true
+					},
+					{
+						"answer": "263",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is 79 - 21",
+				"answers": [{
+						"answer": "67",
+						"feedback": false
+					},
+					{
+						"answer": "69",
+						"feedback": false
+					},
+					{
+						"answer": "68",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
